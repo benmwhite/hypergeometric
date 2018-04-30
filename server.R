6db9270dee49f94c2b99301449ff1d93101ff56b
Benjamin White <benjamin.malilay.white@gmail.com>
2018-04-29 21:46:45 -0500

    Update server.R
    adjusted graphical parameters

--- server.R
+++ server.R
@@ -6,7 +6,7 @@ shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
     x    <- 1:20
     probs <- dhyper(x, input$M, input$N, input$K)
-    qplot(x, probs, geom = c("point", "line"), ylab = "probability", ylim = c(0, 1),
+    qplot(x, probs, geom = c("point", "line"), ylab = "probability", ylim = c(0, 0.75),
          main = "Probability of drawing exactly x white balls")
   })
 })
