67f82ff1ebdcdac1ba6d22387ba8776d33e90b37
Benjamin White <benjamin.malilay.white@gmail.com>
2018-04-29 21:44:42 -0500

    Update server.R
    updated for slider adjustments

--- server.R
+++ server.R
@@ -2,12 +2,11 @@ library(shiny)
 library(ggplot2)
 
 shinyServer(function(input, output) {
-
+  #plot output
   output$distPlot <- renderPlot({
     x    <- 1:20
-    probs <- dhyper(x, input$M, input$N - input$M, input$n)
-    qplot(x, probs, geom = c("point", "line"), ylab = "p(x)", ylim = c(0, 1))
-
+    probs <- dhyper(x, input$M, input$N, input$K)
+    qplot(x, probs, geom = c("point", "line"), ylab = "probability", ylim = c(0, 1),
+         main = "Probability of drawing exactly x white balls")
   })
-
 })
